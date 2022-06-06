aa6386ca5b75343bcd8fdf2d3d79206506cb758e
20373313lc <20373313@buaa.edu.cn>
2022-06-06 20:03:22 +0800

    music

--- src/exec/main/GameStart.java
+++ src/exec/main/GameStart.java
@@ -1,4 +1,16 @@
 package exec.main;
 
+import exec.frame.GameFrame;
+import exec.frame.GameJPanel;
+import exec.thread.MusicThread;
+
 public class GameStart {
+    public static <GameMusicPlayer> void main(String[] args) {
+        GameFrame frame = new GameFrame();
+        GameJPanel panel = new GameJPanel();
+        GameControl control = new GameControl();
+
+        MusicThread musicPlayer = new MusicThread();
+        musicPlayer.start();
+    }
 }
