40393b7734f6562aacb7749d30b1dbfff52eccd1
20373313lc <20373313@buaa.edu.cn>
2022-06-06 20:10:45 +0800

    music

--- src/exec/main/GameStart.java
+++ src/exec/main/GameStart.java
@@ -4,6 +4,13 @@ import exec.frame.GameFrame;
 import exec.frame.GameJPanel;
 import exec.thread.MusicThread;
 
+/**
+ * @author :lc
+ * @date :2022-6-6
+ * @Classname :游戏开始
+ * @Description :游戏的启动入口
+ * The main class of the game.
+ */
 public class GameStart {
     public static <GameMusicPlayer> void main(String[] args) {
         GameFrame frame = new GameFrame();
