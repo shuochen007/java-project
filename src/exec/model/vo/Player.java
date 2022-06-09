04deaba14834efda1fe1ccbf69533e0f3684f7f6
shuochen007 <1264133835@qq.com>
2022-06-09 20:49:14 +0800

    lsc 06092049 update the Player class

--- src/exec/model/vo/Player.java
+++ src/exec/model/vo/Player.java
@@ -1,10 +1,34 @@
 package exec.model.vo;
 
+import java.util.HashMap;
+
 /**
  * 玩家类
  * @author lsc
  * 主要涉及玩家的相关操作及规范
  */
+//只是不报错,完工时不用这个
+class Soldier {
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
+
+class Celebrity{
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
 
 public class Player {
     //玩家生命值
@@ -23,6 +47,16 @@ public class Player {
     private int level;
     //玩家目前经验值
     private int exp;
+    //玩家场上棋子容量
+    private int boardCapacity;
+    //玩家场上棋子数量
+    private int onBoardNum;
+    //玩家备战席棋子数量
+    private int onPrepareNum;
+    //玩家持有的棋子队列,key=棋子,value=isBoard
+    private HashMap<Soldier, Boolean> soldierList = new HashMap<>();
+    //玩家拥有的伟人
+    private Celebrity celebrity;
     //构造函数
     public Player(boolean AI){
         this.health = 100;
@@ -33,6 +67,10 @@ public class Player {
         this.goldPerRound = 1;
         this.level = 1;
         this.exp = 0;
+        this.soldierList = null;
+        this.celebrity = null;
+        this.onPrepareNum = 0;
+        this.onBoardNum = 0;
     }
     //以下为get和set相关属性的方法(boolean型是is和set)
     public int getHealth() {
@@ -78,17 +116,38 @@ public class Player {
     }
 
     public int getLevel() {
-        return level;
+        return this.level;
     }
     public void setLevel(int level) {
         this.level = level;
     }
 
     public int getExp() {
-        return exp;
+        return this.exp;
     }
     public void setExp(int exp) {
         this.exp = exp;
+    }
+
+    public int getBoardCapacity() {
+        return this.boardCapacity;
+    }
+    public void setBoardCapacity(int boardCapacity) {
+        this.boardCapacity = boardCapacity;
+    }
+
+    public int getOnBoardNum() {
+        return onBoardNum;
+    }
+    public void setOnBoardNum(int onBoardNum) {
+        this.onBoardNum = onBoardNum;
+    }
+
+    public int getOnPrepareNum() {
+        return onPrepareNum;
+    }
+    public void setOnPrepareNum(int onPrepareNum) {
+        this.onPrepareNum = onPrepareNum;
     }
 
     //战斗结束之后，根据这场战斗结果，计算出新的最近战绩
@@ -134,8 +193,11 @@ public class Player {
     //升级(单纯的结果)
     public void upLevel(){
         int beforeLevel = getLevel();
-        int nowLevel = beforeLevel++;
+        int nowLevel = beforeLevel + 1;
+        int beforeCapacity = getBoardCapacity();
+        int nowCapacity = beforeCapacity + 1;
         setLevel(nowLevel);
+        setBoardCapacity(nowCapacity);
     }
     //购买经验值(内部进行条件判断，满足条件则调用upLevel)
     public void buyExp() {
@@ -180,5 +242,104 @@ public class Player {
         int beforeGold = this.getGold();
         int goldThisRound = this.goldPerRound + getInterest() + getResultsGold();
         setGold(beforeGold + goldThisRound);
+    }
+    //购买棋子,备战席容量为9
+    public void buySoldier(Soldier soldier) {
+        if(getOnPrepareNum() <= 9) {
+            int beforeGold = getGold();
+            if(beforeGold >= soldier.getValue()) {
+                int afterGold = beforeGold - soldier.getValue();
+                setGold(afterGold);
+                this.soldierList.put(soldier, false);
+            }
+            else{
+                //TODO 您的金币不足！
+            }
+        }
+        else{
+            //TODO 备战席已满，无法再容纳更多棋子！
+        }
+    }
+    //出战
+    public void prepareToBoard(Soldier soldier) {
+        int beforePrepare = getOnPrepareNum();
+        int beforeBoard = getOnBoardNum();
+        if(beforeBoard < this.boardCapacity) {
+            int afterPrepare = beforePrepare - 1;
+            setOnPrepareNum(afterPrepare);
+            int afterBoard = beforeBoard + 1;
+            setOnBoardNum(afterBoard);
+            this.soldierList.put(soldier,true);
+        }
+        else{
+            //TODO 场上已满，无法再容纳更多棋子！
+        }
+    }
+    //休战
+    public void boardToPrepare(Soldier soldier){
+        int beforePrepare = getOnPrepareNum();
+        int beforeBoard = getOnBoardNum();
+        if(beforePrepare < 9) {
+            int afterBoard = beforeBoard - 1;
+            setOnBoardNum(afterBoard);
+            int afterPrepare = beforePrepare + 1;
+            setOnPrepareNum(afterPrepare);
+            this.soldierList.put(soldier,false);
+        }
+        else{
+            //TODO 备战席已满，无法再容纳更多棋子！
+        }
+    }
+    //出售棋子
+    public void soldSoldier(Soldier soldier) {
+        int beforePrepare = getOnPrepareNum();
+        int afterPrepare = beforePrepare - 1;
+        setOnPrepareNum(afterPrepare);
+        this.soldierList.remove(soldier);
+        int beforeGold = getGold();
+        int afterGold = beforeGold + soldier.getValue();
+        setGold(afterGold);
+    }
+    //购买伟人
+    public void buyCelebrity(Celebrity celebrity){
+        if(this.celebrity == null){
+            int beforeGold = getGold();
+            if(beforeGold >= celebrity.getValue()) {
+                int afterGold = beforeGold - celebrity.getValue();
+                setGold(afterGold);
+                this.celebrity = celebrity;
+            }
+            else{
+                //TODO 您的金币不足！
+            }
+        }
+        else{
+            //TODO 您只能拥有一个伟人！
+        }
+    }
+    //出售伟人
+    public void soldCelebrity(Celebrity celebrity){
+        int beforeGold = getGold();
+        int afterGold = beforeGold + celebrity.getValue();
+        setGold(afterGold);
+        this.celebrity = null;
+    }
+    //战斗结束后受到伤害
+    public void getHurt(int enemyAliveNum) {
+        int beforeHealth = getHealth();
+        int afterHealth = beforeHealth - (enemyAliveNum * this.getLevel());
+        setHealth(afterHealth);
+    }
+    //刷新商店
+    public void refreshShop() {
+        int beforeGold = getGold();
+        if(beforeGold >= 2){
+            int afterGold = beforeGold - 2;
+            setGold(afterGold);
+            //TODO 刷新商店
+        }
+        else{
+            //TODO 您的金币不足！
+        }
     }
 }
